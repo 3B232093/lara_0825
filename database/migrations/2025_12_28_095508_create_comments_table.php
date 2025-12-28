e3d247bff66bf3618dc22087b8148e89c0f9efee
3B232093 <3b232093@gm.student.ncut.edu.tw>
2025-12-28 17:56:20 +0800

    新增另一個migration

--- database/migrations/2025_12_28_095508_create_comments_table.php
+++ database/migrations/2025_12_28_095508_create_comments_table.php
@@ -11,20 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
             $table->timestamps();
         });
     }
-
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
